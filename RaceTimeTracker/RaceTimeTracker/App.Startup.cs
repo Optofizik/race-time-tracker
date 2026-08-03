b65b8bcdde1dec6544751ea0169fc335aba52d1f
Dmytro Yakymov <Dmytro.Yakymov@iteam.gr>
2026-08-03 22:49:50 +0300

    Complete task #1 - Bootstrap solution and complete it

---
+++ RaceTimeTracker/RaceTimeTracker/App.Startup.cs
@@ -0,0 +1,31 @@
+using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RaceTimeTracker;
+
+public partial class App
+{
+    private ServiceProvider? serviceProvider;
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        var services = new ServiceCollection();
+        ConfigureServices(services);
+
+        serviceProvider = services.BuildServiceProvider();
+        serviceProvider.GetRequiredService<MainWindow>().Show();
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        serviceProvider?.Dispose();
+        base.OnExit(e);
+    }
+
+    private static void ConfigureServices(IServiceCollection services)
+    {
+        services.AddSingleton<MainWindow>();
+    }
+}
